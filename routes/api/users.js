daa41f60c97c071ff89702e115fae3848a5fcb7e
zyrave <rob.rivermark@gmail.com>
2018-05-07 13:42:55 +0700

    Add register user api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 const router = express.Router();
 
@@ -6,5 +10,39 @@ const router = express.Router();
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    }
+    const avatar = gravatar.url(req.body.email, {
+      s: '200', // Size
+      r: 'pg', // Rating
+      d: 'mm', // Default
+    });
+
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      avatar,
+      password: req.body.password,
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(result => res.json(result))
+          .catch(err => console.log(err));
+      });
+    });
+  });
+});
 
 module.exports = router;
